a3e569ae50a5d3a74321177c0ae622a95f540c9c
silvertejp89 <kriss.stockhaus@gmail.com>
2022-08-12 13:54:04 +0200

    added presentation section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
+import Presentation from './components/Presentation';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Presentation />
       <Body />
       <a
         className="App-link"
---
+++ src/components/Presentation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import lineDot from '../assets/lineDot.png';
+
+function Presentation() {
+  return (
+    <section className="intro-section">
+      <div className="container">
+        <p className="intro-text">
+          <img className="line-dot-icon" src={lineDot} alt="line and dot" />
+          Hello! I'm a front end developer with a passion code and usability! I
+          Love to create products and user experiences that is easy to
+          understand and fun to use. My background consists of many years of
+          experience in information design such as technical information with
+          focus on illustration. Key competence: Javascript, React, Redux &
+          Illustrator.
+        </p>
+      </div>
+      <a
+        href="https://www.krissstockhaus.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p class="illustration-link">
+          Curious about my illustrations? Check them out here!
+        </p>
+      </a>
+    </section>
+  );
+}
+
+export default Presentation;
