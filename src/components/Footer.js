5dcbeee17b6992842cfcb6e1872ac58d791d1be3
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2025-06-20 23:50:17 +0200

    list changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer">
       <h3 className="contact-head">contact</h3>
       {/* <p className="contact-name">Kriss Stockhaus</p> */}
-      <p className="contact-email">kriss.stockhaus@medieinstitutet.se</p>
+      <p className="contact-email">kriss.stockhaus@brinelinegames.se</p>
     </footer>
   );
 };
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,34 +12,12 @@ const Skills = () => {
       <div className="container">
         <div className="skill-list">
           <ul className="code-skills">
-            <li className="list-head">Code</li>
-            <li>HTML</li>
-            <li>CSS & SCSS</li>
-            <li>JavaScript ES6</li>
-            <li>TypeScript</li>
-            <li>React</li>
-            {/* '            <li>Styled Components</li>' */}
-            <li>Github</li>
-            <li>Redux</li>
-            <li>Node.js</li>
-            <li>SQL</li>
-          </ul>
-          <ul className="toolbox-skills">
-            <li className="list-head">Toolbox</li>
-            <li>Postman</li>
-            <li>Adobe Photoshop</li>
-            <li>Adobe Illustrator</li>
-            <li>Adobe XD</li>
-            <li>IsoDraw</li>
-            <li>Figma</li>
-            <li>Slack</li>
-            <li>Visual Studio Code</li>
-          </ul>
-          <ul className="upcoming-skills">
-            <li className="list-head">Upcoming</li>
-            <li>PHP</li>
-            <li>Bootstrap</li>
-            <li>jQuery</li>
+            {/* <li className="list-head">Code</li> */}
+            <li>UX & UI with accessibility focus</li>
+            <li>Agile workflows & team collaboration</li>
+            <li>Clear communication & structured thinking</li>
+            <li>Visual storytelling & fact-based design</li>
+            <li>Maintainable code & continuous learning</li>
           </ul>
         </div>
       </div>
