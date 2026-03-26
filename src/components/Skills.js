3527cd773f0e3174d1c044c4a5cf0294ab88c87f
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2026-03-26 11:43:26 +0100

    skills to work approach

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,16 +7,13 @@ const Skills = () => {
   return (
     <section className="portfolio-section">
       <div className="heading-container">
-        <h2>Skills</h2>
+        <h2>Work Approach</h2>
       </div>
       <div className="container">
         <div className="skill-list">
           <ul className="toolbox-skills">
-            <li className="list-head">Toolbox</li>
-            <li>Maintainable code & continuous learning</li>
+            <li>Maintainable code & continuous learning </li>
             <li>Clear communication & structured thinking</li>
-          </ul>
-          <ul className="upcoming-skills">
             <li>Agile workflows & team collaboration</li>
             <li>UX & UI with accessibility focus</li>
           </ul>
